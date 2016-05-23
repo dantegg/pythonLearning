5878613b4f5c9f8e3413ce0dc03bcdd5a36ad05e
dantegg <luroufan327@hotmail.com>
2016-05-24 00:44:22 +0800

    a little change

--- pyWeiboSpider.py
+++ pyWeiboSpider.py
@@ -7,7 +7,8 @@ import urllib
 url = 'http://www.server.com/login'
 user_agent= 'Mozilla/4.0(compatible;MSIE 5.5;Windows NT)'
 values={'username':'zzz',
-        'password':'zzz'}
+        'password':'zzz',
+        'ec':'0'}
 headers = {'User-Agent':user_agent}
 data = urllib.urlencode(values)
 request = urllib2.Request(url,data,headers)
