bdc09d5e73f9415930d3155ccf6a19cdf7a4e19b
dantegg <runan.zhang@ele.me>
2017-05-17 11:49:31 +0800

    update

--- listgenerator_practice.py
+++ listgenerator_practice.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env pthon3
 # -*- coding: utf-8 -*-
 
-def triangles():
-    pass
 
+def triangles():
+    L = [1]
+    while True:
+        yield L
+        L.append(0)
+        L = [L[i-1] + L[i] for i in range(len(L))]
 n = 0
 for t in triangles():
     print(t)
