5527d81d12c587cca7a71d7fbfb9fc0e64486814
dantegg <luroufan327@hotmail.com>
2017-05-29 00:47:47 +0800

    'update'

--- fileOpen.py
+++ fileOpen.py
@@ -2,7 +2,26 @@
 # -*- coding: utf-8 -*-
 
 f = open('test.txt', "w")
-
+print(f.closed)
 f.write('this is a test file')
 
 f.close()
+print(f.closed)
+
+
+class Vow(object):
+    def __init__(self, text):
+        self.text = text
+
+    def __enter__(self):
+        self.text = "I say: " + self.text
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.text = self.text + '!'
+
+
+with Vow("I'm fine") as myVow:
+    print(myVow.text)
+
+print(myVow.text)
