dbf077bc0518df8185bb5f3a8bf64a18c9487213
dantegg <runan.zhang@ele.me>
2017-07-26 10:57:27 +0800

    update

--- multiProcessLearning.py
+++ multiProcessLearning.py
@@ -4,3 +4,5 @@ print('Process (%s) start...' %os.getpid())
 pid = os.fork()
 if pid == 0:
     print('I am child process (%s) and my parent is %s' %(os.getpid(), os.getppid()))
+else:
+    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))    
