bf23ca79312772e4f71cc8fa44cd1da6ee23d8bc
dantegg <runan.zhang@ele.me>
2017-08-10 15:13:30 +0800

    update

---
+++ pillowLearn.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+# 打开一个jpg图像文件，注意是当前路径
+im = Image.open('test.jpg')
+
+#获得图像尺寸
+w, h = im.size
+print('Original image size: %s X %s' % (w, h))
+
+#缩放到50%
+im.thumbnail((w//2, h//2))
+print('Resize image to %s X %s' % (w//2, h//2))
+
+im.save('thumbnail.jpg', 'jpeg')
