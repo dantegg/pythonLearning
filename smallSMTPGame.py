8b7530bab56c8f86a6778e98f93d664197b57e2e
dantegg <luroufan327@hotmail.com>
2017-09-07 00:07:31 +0800

    update

--- smallSMTPGame.py
+++ smallSMTPGame.py
@@ -1,5 +1,21 @@
-import datetime
-#获取当前时间
+import time
+# 利用sched方法来执行定时任务
+import sched
 
 def run_Task():
     print('hahaha')
+
+schedule = sched.scheduler(time.time, time.sleep)
+
+def printTime(string1, float1):
+    print("now is ", time.time(), "| output = ", string1, float1)
+
+print('===========',time.time(),'==========')
+schedule.enter(2,0, printTime, ("test1", time.time()))
+schedule.enter(2,0, printTime, ("test1", time.time()))
+schedule.enter(3,0, printTime, ("test1", time.time()))
+schedule.enter(4,0, printTime, ("test1", time.time()))
+schedule.run()
+print('===========',time.time(),'==========')
+
+
